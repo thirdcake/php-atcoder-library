8ab91e1ae0d42897d1fbddfece07fba9adedc7bb
thirdcake <ksktnk768@gmail.com>
2025-02-10 18:17:12 +0900

    change 0-index

--- fenwick_tree.php
+++ fenwick_tree.php
@@ -1,5 +1,4 @@
 <?php
-
 // Fenwick Tree
 class Fenwick {
     public array $tree;
@@ -7,18 +6,19 @@ class Fenwick {
 
     public function __construct(int $n) {
         $this->size = $n;
-        $this->tree = array_fill(0, $n + 1, 0);
+        $this->tree = array_fill(0, $n, 0);
     }
-
+	// $index は、0-index
     public function add(int $index, int $value): void {
-        for ($i=$index; $i<$this->size; $i+=($i & -$i)) {
+        for ($i=$index; $i<$this->size; $i|=$i + 1) {
             $this->tree[$i] += $value;
         }
     }
-
-    public function sum(int $index): int {
-        $sum = 0;
-        for($i=$index; $i>0; $i-=($i & -$i)) {
+	// [0, $a) の区間和。0-index, 0は含む、$aは含まない
+	// [$a, $b) は、$this->sum($b) - $this->sum($a);
+    public function sum(int $a): int {
+		$sum = 0;
+        for($i=$a-1; $i>=0; $i=($i & ($i+1))-1) {
             $sum += $this->tree[$i];
         }
         return $sum;
@@ -28,12 +28,14 @@ class Fenwick {
 
 // 使い方
 $fen = new Fenwick(5);
+$fen->add(0, 1);
 $fen->add(1, 1);
 $fen->add(2, 1);
 $fen->add(3, 1);
 $fen->add(4, 1);
-echo $fen->sum(1).PHP_EOL;
-echo $fen->sum(2).PHP_EOL;
-echo $fen->sum(3).PHP_EOL;
-echo $fen->sum(4).PHP_EOL;
+echo '[1, 1, 1, 1, 1], 0-index'.PHP_EOL;
+echo '[0,1)の和: '. $fen->sum(1).PHP_EOL;
+echo '[0,2)の和: '. $fen->sum(2).PHP_EOL;
+echo '[0,4)の和: '. $fen->sum(4).PHP_EOL;
+echo '[0,5)の和: '. $fen->sum(5).PHP_EOL;
 
